c47d12d3f0434dd77580f2c25b698af8264fb97f
torezani89 <torezani89@outlook.com>
2021-10-13 19:13:40 -0300

    Update ContaCorrente.js

--- Contas/ContaCorrente.js
+++ Contas/ContaCorrente.js
@@ -1,21 +1,20 @@
-import {Cliente} from "./Cliente.js"
 import {Conta} from "./Conta.js"
 
 export class ContaCorrente extends Conta {
     static numeroDeContas = 0;
     // agencia;
-    // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
+    // #saldo = 0 https://github.com/tc39/proposal-class-fields#private-fields
     // _cliente;
     // _saldo = 0;
 
-    constructor(cliente,agencia) {
-        super(cliente,agencia)
+    constructor(cliente, agencia) {
+        super(cliente, agencia);
         this.tipo = "corrente";
         ContaCorrente.numeroDeContas = ContaCorrente.numeroDeContas + 1;
     }
 
-    sacar(valor){
-        let taxa = 1.01;
+    sacar(valor) {
+        let taxa = 1.02;
         return this._sacar(valor, taxa);
     }
 
